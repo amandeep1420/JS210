44c9faa2b62b2378a082558a51078cefd11d339c
amandeep1420 <amandeep1420@gmail.com>
2021-11-22 01:58:14 +0000

    Finished section on hoisting.

---
+++ L2/hoisting.js
@@ -0,0 +1,34 @@
+// var foo;
+// var bar = 'qux';
+
+
+// let food;
+// let bard = "qux";
+// const baz = 3.1415;
+
+// function meow() {
+//   if (true) {
+//     var b = 'meow';
+//     let c = 'meower';
+//   }
+//   console.log(b);
+//   console.log(c);
+// }
+
+// console.log(aVar);
+
+// var aVar = 4;
+
+// console.log(aLet);
+
+// let aLet = 'meow';
+
+
+bar();              // logs undefined
+var foo = 'hello';
+
+function bar() {
+  console.log(foo);
+}
+
+bar();
